176ae0092554eb4af62d4b0c07fa215fe35cd543
bethrezen <b37hr3z3n@gmail.com>
2016-08-22 16:42:05 +0300

    upd8 composer.json and dev-symlinks helper

--- dev-symlinks.sh
+++ dev-symlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKAGES='yii2-admin-utils
+DEVGROUP_PACKAGES='yii2-admin-utils
 yii2-multilingual
 yii2-polyglot
 yii2-deferred-tasks
@@ -15,27 +15,25 @@ yii2-jsoneditor
 yii2-media-storage
 yii2-measure
 yii2-users-module'
-
 CURRENT_DIRECTORY=`pwd`
 
-for Package in ${PACKAGES}
+for Package in ${DEVGROUP_PACKAGES}
 do
     cd ../${Package}
     echo "Updating git $Package"
     git pull
-
 done
 
 cd ${CURRENT_DIRECTORY}
 
-for Package in ${PACKAGES}
+for Package in ${DEVGROUP_PACKAGES}
 do
     rm -rf vendor/devgroup/${Package}
 done
 
-composer update
+composer update -vvv
 
-for Package in ${PACKAGES}
+for Package in ${DEVGROUP_PACKAGES}
 do
     rm -rf vendor/devgroup/${Package}
     ln -s ../../../${Package} vendor/devgroup/
@@ -43,6 +41,43 @@ done
 
 rm -rf vendor/devgroup/bh
 ln -s ../../../bh-php vendor/devgroup/bh
+
 rm -rf vendor/dotplant/monster
 ln -s ../../../monster vendor/dotplant/monster
+
+rm -rf vendor/dotplant/content
+ln -s ../../../dotplant-content vendor/dotplant/content
+
+rm -rf vendor/dotplant/entity-structure
+ln -s ../../../dotplant-entity-structure vendor/dotplant/entity-structure
+
 ls -la vendor/devgroup
+
+./yii extension/update-config
+
+EXTENSIONS='dotplant/content
+dotplant/entity-structure
+dotplant/monster
+devgroup/yii2-admin-utils
+devgroup/yii2-data-structure-tools
+devgroup/yii2-deferred-tasks
+devgroup/yii2-entity
+devgroup/yii2-events-system
+devgroup/yii2-extensions-manager
+devgroup/yii2-frontend-utils
+devgroup/yii2-intent-analytics
+devgroup/yii2-jsoneditor
+devgroup/yii2-jstree-widget
+devgroup/yii2-measure
+devgroup/yii2-media-storage
+devgroup/yii2-multilingual
+devgroup/yii2-polyglot
+devgroup/yii2-tag-dependency-helper
+devgroup/yii2-users-module'
+
+for Ext in ${EXTENSIONS}
+do
+    ./yii extension/activate ${Ext}
+done
+
+./yii migrate
